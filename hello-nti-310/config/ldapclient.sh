3c0b4af7d366b1443815fbea01dec5f797fbe428
dakoda17 <kodamondragon@gmail.com>
2019-02-21 18:32:33 -0800

    Update ldapclient.sh

--- hello-nti-310/config/ldapclient.sh
+++ hello-nti-310/config/ldapclient.sh
@@ -1,52 +1,55 @@
 #!/bin/bash
-#this is my file for the document ldapclient.sh with some additions
-# this doesn't reallly work it has some fixes to it: https://www.tecmint.com/configure-ldap-client-to-connect-external-authentication/ 
-# it requires that my ldapc server is running
+# based off of https://www.tecmint.com/configure-ldap-client-to-connect-external-authentication
+# with some additions that make it work, as opposed to not work
 
-#in the client
+if [ -e /etc/ldap.secret]; then
+exit 0;
+fi
+
 apt-get update
 apt-get install -y debconf-utils
 export DEBIAN_FRONTEND=noninteractive
 apt-get --yes install libnss-ldap libpam-ldap ldap-utils nscd
 unset DEBIAN_FRONTEND
 
-#cd to root to make a tempfile like this: cd ~ : then
-#vim /tempfile: copied over the ldap carrot text without a carrot into the tempfile
-#now: while read line; do echo "$line" | debconf-set-selections; done < /tempfile
-#now: echo "password > /etc/ldap.secret we still have to echho the root passwd into root/etc/password echo passoword into ldap secret
 
-#not sure if i still need this: sudo apt update && sudo apt install -y libnss-ldap libpam-ldap ldap-utils nscd
+echo "ldap-auth-config ldap-auth-config/bindpw password
+nslcd nslcd/ldap-bindpw password
+ldap-auth-config ldap-auth-config/rootbindpw password
+ldap-auth-config ldap-auth-config/move-to-debconf boolean true
+nslcd nslcd/ldap-sasl-krb5-ccname string /var/run/nslcd/nslcd.tkt
+nslcd nslcd/ldap-starttls boolean false
+libpam-runtime libpam-runtime/profiles multiselect unix, ldap, systemd, capability
+nslcd nslcd/ldap-sasl-authzid string
+ldap-auth-config ldap-auth-config/rootbinddn string cn=ldapadm,dc=nti310,dc=local
+nslcd nslcd/ldap-uris string ldap://ldapc
+nslcd nslcd/ldap-reqcert select
+nslcd nslcd/ldap-sasl-secprops string
+ldap-auth-config ldap-auth-config/ldapns/ldap_version select 3
+ldap-auth-config ldap-auth-config/binddn string cn=proxyuser,dc=example,dc=net
+nslcd nslcd/ldap-auth-type select none
+nslcd nslcd/ldap-cacertfile string /etc/ssl/certs/ca-certificates.crt
+nslcd nslcd/ldap-sasl-realm string
+ldap-auth-config ldap-auth-config/dbrootlogin boolean true
+ldap-auth-config ldap-auth-config/override boolean true
+nslcd nslcd/ldap-base string dc=nti310,dc=local
+ldap-auth-config ldap-auth-config/pam_password select md5
+nslcd nslcd/ldap-sasl-mech select
+nslcd nslcd/ldap-sasl-authcid string
+ldap-auth-config ldap-auth-config/ldapns/base-dn string dc=nti310,dc=local
+ldap-auth-config ldap-auth-config/ldapns/ldap-server string ldap://ldapc/
+nslcd nslcd/ldap-binddn string
+ldap-auth-config ldap-auth-config/dblogin boolean false" >> tempfile
 
+while read line; do echo "$line" | debconf-set-selections; done < tempfile
 
-#format of sed: sed -i 's/thing1/thing2/g' OR
-#sed -i(insert) 's(search)$(for)thing1&
-#we used this line: debconf-get-selections | grep ^ldap
+echo "OkBWKtZ9" > /etc/ldap.secret
 
+chown 600 /etc/ldap.secret
 
-#get rid of the i in ladpi add the name of my server: ldap://ldapc/
-#next screen: dc-nti310,dc=local , 3, yes, no 
+sudo auth-client-config -t nss -p lac_ldap
 
-#in the sever
-#for manager account go to https on the ldap server go to the ip, remove the s /phpldapadmin - take the entire line from the login and paste it
-#sudo su into the server cat /root/ldap_admin_pass copy the password into the client and OK
+echo "account sufficient pam_succeed_if.so uid = 0 use_uid quiet" >> /etc/pam.d/su
 
-#in the client
-#paste in password for the manager account
-#from the linked file (name server switch) paste in:
-sudo auth-client-config -t nss -p lac_ldap 
-sudo pam-auth-update 
-
-# 1-3 and 5 are already starred by default are therefore we dont' need line 11
-
-sudo systemctl restart nscd
-sudo systemctl enable nscd
-
-#vim /etc/pam.d/su - then paste: account sufficient pam_succeed_if.so uid = 0 use_uid quiet - as line 7 under line 6 
-# or echo " sufficient pam_succeed_if.so uid = 0 use_uid quiet" >> /etc/pam.d/su
-# vim /etc/ldap/ldap.conf
-#uncomment BASE and URI
-# make this change: BASE    dc=nti310,dc=local  and URI     ldap://ldapc
-#getent passwd - this will show us the uses we created
-#now test loging in to one of the users via su like this: su - Person1
-#the result should be: No directoryy, loggin in with HOME=/
-#exit to return to cl
+systemctl restart nscd
+systemctl enable nscd
