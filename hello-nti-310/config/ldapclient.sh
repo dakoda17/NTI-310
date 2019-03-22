4a01ae0f339498855ad7382558b8f6389246289e
Dakoda <kodamondragon@gmail.com>
2019-03-21 21:28:15 -0700

    Update ldapclient.sh

--- hello-nti-310/config/ldapclient.sh
+++ hello-nti-310/config/ldapclient.sh
@@ -22,7 +22,7 @@ nslcd nslcd/ldap-starttls boolean false
 libpam-runtime libpam-runtime/profiles multiselect unix, ldap, systemd, capability
 nslcd nslcd/ldap-sasl-authzid string
 ldap-auth-config ldap-auth-config/rootbinddn string cn=ldapadm,dc=nti310,dc=local
-nslcd nslcd/ldap-uris string ldap://continue_ldap_install
+nslcd nslcd/ldap-uris string ldap://ldap2
 nslcd nslcd/ldap-reqcert select
 nslcd nslcd/ldap-sasl-secprops string
 ldap-auth-config ldap-auth-config/ldapns/ldap_version select 3
@@ -37,7 +37,7 @@ ldap-auth-config ldap-auth-config/pam_password select md5
 nslcd nslcd/ldap-sasl-mech select
 nslcd nslcd/ldap-sasl-authcid string
 ldap-auth-config ldap-auth-config/ldapns/base-dn string dc=nti310,dc=local
-ldap-auth-config ldap-auth-config/ldapns/ldap-server string ldap://continue_ldap_install/
+ldap-auth-config ldap-auth-config/ldapns/ldap-server string ldap://ldap2/
 nslcd nslcd/ldap-binddn string
 ldap-auth-config ldap-auth-config/dblogin boolean false" >> tempfile
 
@@ -52,7 +52,7 @@ sudo auth-client-config -t nss -p lac_ldap
 echo "account sufficient pam_succeed_if.so uid = 0 use_uid quiet" >> /etc/pam.d/su
 
 sed -i 's/base dc=example,dc=net/base dc=nti310,dc=local/g' /etc/ldap.conf
-sed -i 's,uri ldapi:///,uri ldap://continue_ldap_install/,g' /etc/ldap.conf
+sed -i 's,uri ldapi:///,uri ldap://ldap2/,g' /etc/ldap.conf
 sed -i 's/rootbinddn cn=manager,dc=example,dc=net/rootbinddn cn=ldapadm,dc=nti310,dc=local/g' /etc/ldap.conf
 
 systemctl restart nscd
